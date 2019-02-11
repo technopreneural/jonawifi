90b797c358ee2398a133c93f8697e4c6ed18b016
technopreneural <technopreneural@yahoo.com>
2019-02-11 23:58:25 +0800

    +launch,+actions,+open.db

--- ap/open/open.py
+++ ap/open/open.py
@@ -3,7 +3,7 @@
 #
 # Libraries
 #
-import subprocess
+from subprocess import Popen
 import time
 import os
 
@@ -12,7 +12,7 @@ import os
 #
 AP_PATH = os.path.dirname(os.path.abspath(__file__))
 AP_CONFIG = AP_PATH + "/open.conf"
-AP_EVENTS = AP_PATH + "/open.events"
+AP_ACTIONS = AP_PATH + "/actions"
 
 IFACE = "wlan0"
 IPADDR = "192.168.253.1/24"
@@ -24,30 +24,30 @@ def dhcp_service_reset():
 	f.write("dhcp-authoritative\r\n")
 	f.write("dhcp-range=192.168.253.2,192.168.253.254,1h\r\n")
 	f.close();
-	subprocess.call('service dnsmasq restart'.split())
+	Popen('service dnsmasq restart'.split(),shell=False)
 	print("DHCP Service reset.")
 
 def interface_wireless_reset():
 	print("Resetting wireless interface...")
-	subprocess.call(('ip link set %s down' % IFACE).split())
-	subprocess.call(('ip addr flush dev %s' % IFACE).split())
-	subprocess.call(('ip link set %s up' % IFACE).split())
-	subprocess.call(('ip addr add %s dev %s' %(IPADDR,IFACE)).split())
+	Popen(('ip link set %s down' % IFACE).split(),shell=False)
+	Popen(('ip addr flush dev %s' % IFACE).split(),shell=False)
+	Popen(('ip link set %s up' % IFACE).split(),shell=False)
+	Popen(('ip addr add %s dev %s' %(IPADDR,IFACE)).split(),shell=False)
 	print("Wireless interface reset.")
 
 def ap_activate():
 	print("Activating access point...")
 	dhcp_service_reset()
 	interface_wireless_reset()
-	subprocess.call(['hostapd','-B',AP_CONFIG])
+	Popen(['hostapd','-B',AP_CONFIG],shell=False)
 	time.sleep(1)
-	subprocess.call(['hostapd_cli','-B','-a',AP_EVENTS])
+	hostapd_cli = Popen(['hostapd_cli','-a',AP_EVENTS],shell=False)
 	print("Access point activated.")
 
 def ap_deactivate():
 	print("Dectivating access point...")
-	subprocess.call('sudo killall hostapd')
-	subprocess.call('sudo killall hostapd')
+	Popen('sudo killall hostapd')
+	Popen('sudo killall hostapd')
 	print("Access point deactivated.")
 
 ap_activate()
